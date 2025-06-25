206baab697a8be8769e47e1c95bc9e2c8f429c4b
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-06-25 09:44:30 +0200

    first commit

---
+++ src/main/java/com/api/gestiondetache/GestiondetacheApplication.java
@@ -0,0 +1,13 @@
+package com.api.gestiondetache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondetacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiondetacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gestiondetache/GestiondetacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestiondetache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondetacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
